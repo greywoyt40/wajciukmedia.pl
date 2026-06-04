a25843670fd42ed687b740e4fdaedc1df8aa4e8e
greywoyt <vaitsiuksv@gmail.com>
2026-06-04 21:38:14 +0200

    Update

---
+++ astro.config.mjs
@@ -0,0 +1,4 @@
+
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+export default defineConfig({ integrations: [tailwind()], site: 'https://www.wajciukmedia.pl' });
